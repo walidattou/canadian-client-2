9f510881d6797719b03c30c763da7b9f7f82bce4
laymne-ops <sohixe77@gmail.com>
2025-08-05 15:11:59 +0100

    03

--- src/i18n.js
+++ src/i18n.js
@@ -1,35 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Import translation files
-import enTranslations from './locales/en.json';
 import frTranslations from './locales/fr.json';
 
 const resources = {
-  en: {
-    translation: enTranslations
-  },
   fr: {
     translation: frTranslations
   }
 };
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
+    lng: 'fr',
     fallbackLng: 'fr',
     debug: false,
     
     interpolation: {
       escapeValue: false,
-    },
-    
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'],
     }
   });
 
